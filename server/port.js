2067742f89d28faa0725a1f7e50e0c93350f354f
Turchenko <stepanturchenko@mail.ru>
2020-06-29 15:23:40 +0200

    update all

--- server/port.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-exports.default = process.env.PORT || 6000;
--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import PORT from './port'
+import PORT from './port';
+
 const App = express();
 App.use(express.json());
-App.use(express.urlencode({extended: true}));
+App.use(express.urlencoded({extended: true}));
 App.use(cors());
 
-App.get('/',(req,res)=>{
-  res.send('hello server')
-})
+App.get('/', (req, res) => {
+  res.sendFile(process.cwd()+'/public/index.html');
+});
 
-App.listen(PORT,()=>{
-  console.log('SERVER START ON PORT',PORT)
-})
+App.listen(PORT, () => {
+  console.log('SERVER START ON PORT', PORT);
+});
