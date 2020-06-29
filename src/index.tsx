0ef81dfef69a8e1136a4b44b618af1039c8a00dc
Turchenko <stepanturchenko@mail.ru>
2020-06-29 16:11:50 +0200

    add sass loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import {render} from 'react-dom';
-
+import './styles/index.sass'
 render(<h1>Hello react</h1>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './src/index.tsx',
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js', 'sass', 'scss', 'css']
   },
   output: {
     path: process.cwd() + '/public',
@@ -12,6 +12,21 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       }
     ]
   },
